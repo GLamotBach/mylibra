3965c5ae970fa574c721489295117d4ff2a27b95
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2023-07-03 08:47:35 +0200

    book_review_edit_review_functionality

--- book_reviews/urls.py
+++ book_reviews/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('read/(<int:title_id>)/', views.read_book_view, name='read_book'),
     path('myreadlist/', views.my_read_books_list_view, name='my_read_list'),
     path('addreview/(<int:read_book_id>)/', views.add_review_view, name='add_review'),
+    path('edit_review/(<int:book_review_id>)/', views.edit_review_view, name='edit_review'),
 ]
