ed0c196941229b25fcae03dcbde5edfa722c0785
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2023-08-08 10:35:11 +0200

    bootstrap

--- public_profile/forms.py
+++ public_profile/forms.py
@@ -6,8 +6,8 @@ class UsersPublicProfileForm(forms.ModelForm):
     class Meta:
         model = UsersPublicProfile
         fields = ['public_name', 'location', 'bio', 'image',]
-        labels = {'public_name': 'Nazwa użytkownika',
-                  'location': 'Lokacja',
-                  'bio': 'Bio',
-                  'image': 'Zdjęcie profilowe',
+        labels = {'public_name': 'Nazwa użytkownika:',
+                  'location': 'Lokacja:',
+                  'bio': 'Bio:',
+                  'image': 'Zdjęcie profilowe:',
                   }
